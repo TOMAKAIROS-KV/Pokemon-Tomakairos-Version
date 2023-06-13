83aeaabf4d29bf3ca5c9e27a957a5cccaf5f813f
TOMAKAIROS <132004902+TOMAKAIROS@users.noreply.github.com>
2023-06-13 18:34:53 -0500

    Add files via upload
    I added pokemon's name, lvl, health-bar, exp bar, and damage infliction per hit and exp.
    
    things i still need to do (copied and pasted from my script.
    
    
    //THINGS TO DO!
    // SET UP HEALTH-BAR FOR ENEMY MONSTER!
    // SET ATTACK ANIMATION FOR CHOSEN MONSTER PER ATTACK BUTTON PER MONSTER.
    // SET UP SO THAT ATTACK BUTTONS DAMAGE ENEMY, NOT YOURSELF!
    // SET A DELAY AFTER YOUR ATTACK FOR ENEMY ATTACK TO START AND ANIMATE.
    // SET THE ENEMIES ATTACK TO INFLICT DAMAGE ON YOUR HEALTH.
    // SET IT UP SO EXP IS GAINED PER BATTLE NOT PER HIT.
    // SET UP 3 MATCHES per ENEMY MONSTER
    // SET UP EVOLUTION ALERT
    // SET UP VICTORY SCREEN!
    // SET UP TO GO BACK TO HOME!

--- script.js
+++ script.js
@@ -176,55 +176,177 @@ function startGameAction() {
     isMonster1Shown &&
     (!isMonster2Shown || !isMonster3Shown || !isMonster4Shown)
   ) {
-    section3.style.display = "block";
-    section3.classList.add("visible");
-    imgElementChosen.src = "./images/PikachuBack.gif";
-    chosenMonster.appendChild(imgElementChosen);
-    imgElementEnemy.src = "./images/PrimalKyogre.gif";
-    enemyMonster.appendChild(imgElementEnemy);
-    enemyMonster.style.marginRight = "200px";
-    console.log("BATTLE START! Pikachu!");
+    let electricType1 = document.getElementById("attackBtn1");
+    let electricType2 = document.getElementById("attackBtn2");
+    let electricType3 = document.getElementById("attackBtn3");
+    let electricType4 = document.getElementById("attackBtn4");
+    electricType1.textContent = "volt-tackle";
+    electricType2.textContent = "quick-attack";
+    electricType3.textContent = "thunder-bolt";
+    electricType4.textContent = "iron-tail";
+    monsterBattle1();
   } else if (
     isMonster2Shown &&
     (!isMonster1Shown || !isMonster3Shown || !isMonster4Shown)
   ) {
-    section3.style.display = "block";
-    section3.classList.add("visible");
-    imgElementChosen.src = "./images/CharmanderBack.gif";
-    chosenMonster.appendChild(imgElementChosen);
-    imgElementEnemy.src = "./images/Celebi.gif";
-    enemyMonster.appendChild(imgElementEnemy);
-    enemyMonster.style.marginRight = "";
-    console.log("BATTLE START! Charmander!");
+    let fireType1 = document.getElementById("attackBtn1");
+    let fireType2 = document.getElementById("attackBtn2");
+    let fireType3 = document.getElementById("attackBtn3");
+    let fireType4 = document.getElementById("attackBtn4");
+    fireType1.textContent = "ember";
+    fireType2.textContent = "tackle";
+    fireType3.textContent = "scratch";
+    fireType4.textContent = "fire-punch";
+    monsterBattle2();
   } else if (
     isMonster3Shown &&
     (!isMonster1Shown || !isMonster2Shown || !isMonster4Shown)
   ) {
-    section3.style.display = "block";
-    section3.classList.add("visible");
-    imgElementChosen.src = "./images/SquirtleBack.gif";
-    chosenMonster.appendChild(imgElementChosen);
-    imgElementEnemy.src = "./images/Moltres.gif";
-    enemyMonster.appendChild(imgElementEnemy);
-    enemyMonster.style.marginRight = "200px";
-    console.log("BATTLE START! Squirtle!");
+    let waterType1 = document.getElementById("attackBtn1");
+    let waterType2 = document.getElementById("attackBtn2");
+    let waterType3 = document.getElementById("attackBtn3");
+    let waterType4 = document.getElementById("attackBtn4");
+    waterType1.textContent = "scratch";
+    waterType2.textContent = "bubble";
+    waterType3.textContent = "tackle";
+    waterType4.textContent = "water-gun";
+    monsterBattle3();
   } else if (
     isMonster4Shown &&
     (!isMonster1Shown || !isMonster2Shown || !isMonster3Shown)
   ) {
-    section3.style.display = "block";
-    section3.classList.add("visible");
-    imgElementChosen.src = "./images/BulbasaurBack.gif";
-    chosenMonster.appendChild(imgElementChosen);
-    imgElementEnemy.src = "./images/Suicune.gif";
-    enemyMonster.appendChild(imgElementEnemy);
-    enemyMonster.style.marginRight = "100px";
-    console.log("BATTLE START! Bulbasaur!");
+    let grassType1 = document.getElementById("attackBtn1");
+    let grassType2 = document.getElementById("attackBtn2");
+    let grassType3 = document.getElementById("attackBtn3");
+    let grassType4 = document.getElementById("attackBtn4");
+    grassType1.textContent = "scratch";
+    grassType2.textContent = "vine-whip";
+    grassType3.textContent = "tackle";
+    grassType4.textContent = "razor-leaf";
+    monsterBattle4();
   }
 }
 
-let attackBtn = document.getElementById("attackBtn");
+function monsterBattle1() {
+  section3.style.display = "block";
+  section3.classList.add("visible");
+  imgElementChosen.src = "./images/PikachuBack.gif";
+  chosenMonster.appendChild(imgElementChosen);
+  imgElementEnemy.src = "./images/PrimalKyogre.gif";
+  enemyMonster.appendChild(imgElementEnemy);
+  enemyMonster.style.marginRight = "200px";
+  chosenMonsterName.style.setProperty("--before-content", "'Pikachu'");
+  console.log("BATTLE START! Pikachu!");
+}
 
-function attackAction() {}
+function monsterBattle2() {
+  section3.style.display = "block";
+  section3.classList.add("visible");
+  imgElementChosen.src = "./images/CharmanderBack.gif";
+  chosenMonster.appendChild(imgElementChosen);
+  imgElementEnemy.src = "./images/Celebi.gif";
+  enemyMonster.appendChild(imgElementEnemy);
+  enemyMonster.style.marginRight = "";
+  chosenMonsterName.style.setProperty("--before-content", "'Charmander'");
+  console.log("BATTLE START! Charmander!");
+}
+
+function monsterBattle3() {
+  section3.style.display = "block";
+  section3.classList.add("visible");
+  imgElementChosen.src = "./images/SquirtleBack.gif";
+  chosenMonster.appendChild(imgElementChosen);
+  imgElementEnemy.src = "./images/Moltres.gif";
+  enemyMonster.appendChild(imgElementEnemy);
+  enemyMonster.style.marginRight = "200px";
+  chosenMonsterName.style.setProperty("--before-content", "'Squirtle'");
+  console.log("BATTLE START! Squirtle!");
+}
+
+function monsterBattle4() {
+  section3.style.display = "block";
+  section3.classList.add("visible");
+  imgElementChosen.src = "./images/BulbasaurBack.gif";
+  chosenMonster.appendChild(imgElementChosen);
+  imgElementEnemy.src = "./images/Suicune.gif";
+  enemyMonster.appendChild(imgElementEnemy);
+  enemyMonster.style.marginRight = "100px";
+  chosenMonsterName.style.setProperty("--before-content", "'Bulbasaur'");
+  console.log("BATTLE START! Bulbasaur!");
+}
 
 startGame.addEventListener("click", startGameAction);
+
+let chosenMonsterName = document.getElementById("chosenMonsterName");
+let attackBtn = document.getElementById("attackBtn");
+let healthBar = document.getElementById("healthBar");
+let expBar = document.getElementById("expBar");
+let levelText = document.getElementById("levelText");
+
+let levelResult = 5; // declare variable outside of function in order to continuously update it.
+let healthDepletionMultiplier = 6.5;
+let expMultiplier = 7.5;
+
+const attackBtn1 = document.getElementById("attackBtn1");
+const attackBtn2 = document.getElementById("attackBtn2");
+const attackBtn3 = document.getElementById("attackBtn3");
+const attackBtn4 = document.getElementById("attackBtn4");
+
+function attackAction1() {
+  let damage = Math.floor(Math.random() * healthDepletionMultiplier);
+  let resultHealth = (healthBar.value -= damage);
+  console.log(resultHealth);
+  if (healthBar.value <= 0) {
+    console.log("you lost!");
+    attackBtn.style.pointerEvents = "none";
+    attackBtn2.style.pointerEvents = "none";
+    attackBtn3.style.pointerEvents = "none";
+    attackBtn4.style.pointerEvents = "none";
+  }
+}
+
+function levelAction() {
+  let expGained = Math.floor(Math.random() * expMultiplier);
+  let resultEXP = (expBar.value += expGained);
+  console.log(resultEXP);
+
+  let levelText = document.querySelector("#levelText");
+  console.log("your level is " + levelResult);
+
+  if (expBar.value >= 100) {
+    levelUp();
+    levelText.setAttribute("data-value", levelResult);
+    expBar.value = "0";
+    healthBar.value = "100";
+  }
+}
+
+function levelUp() {
+  levelResult++;
+  if (levelUp) {
+    expMultiplier -= 0.5;
+    console.log("your exp multiplier is" + expMultiplier);
+    healthDepletionMultiplier -= 1;
+    console.log("your damage multiplier is" + healthDepletionMultiplier);
+  }
+  return true;
+}
+
+function combineActions() {
+  attackAction1();
+  levelAction();
+}
+
+attackBtn1.addEventListener("click", combineActions);
+
+//THINGS TO DO!
+// SET UP HEALTH-BAR FOR ENEMY MONSTER!
+// SET ATTACK ANIMATION FOR CHOSEN MONSTER PER ATTACK BUTTON PER MONSTER.
+// SET UP SO THAT ATTACK BUTTONS DAMAGE ENEMY, NOT YOURSELF!
+// SET A DELAY AFTER YOUR ATTACK FOR ENEMY ATTACK TO START AND ANIMATE.
+// SET THE ENEMIES ATTACK TO INFLICT DAMAGE ON YOUR HEALTH.
+// SET IT UP SO EXP IS GAINED PER BATTLE NOT PER HIT.
+// SET UP 3 MATCHES per ENEMY MONSTER
+// SET UP EVOLUTION ALERT
+// SET UP VICTORY SCREEN!
+// SET UP TO GO BACK TO HOME!
--- test.js
+++ test.js
@@ -1,34 +1,3 @@
-function changeImageDown() {
-  trainerElement.src = imagesDown[currentIndex];
-  currentIndex = (currentIndex + 1) % imagesDown.length;
-}
-
-// Function to start the animation for left key
-function startAnimationDown() {
-  animationInterval = setInterval(changeImageDown, 150);
-}
-
-// Function to stop the animation for left key and revert back to the default image
-function stopAnimationDown() {
-  clearInterval(animationInterval);
-  trainerElement.src = "./images/idle-down.png";
-}
-
-// Event listeners for keydown and keyup events for left key
-window.addEventListener("keydown", function (event) {
-  if (
-    (event.key === "s" || event.key === "ArrowDown") &&
-    !isLeftKeyDown &&
-    !isKeyDown
-  ) {
-    isLeftKeyDown = true;
-    startAnimationDown();
-  }
-});
-
-window.addEventListener("keyup", function (event) {
-  if (event.key === "s" || event.key === "ArrowDown") {
-    isLeftKeyDown = false;
-    stopAnimationDown();
-  }
-});
+currentValue += 1;
+let levelResult = currentValue;
+console.log("You leveled up by " + levelResult + "!");
